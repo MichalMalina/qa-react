0f85fc5f4e5f6c0d4e717d55467055529abb5143
MichalMalina <38461440+MichalMalina@users.noreply.github.com>
2020-05-13 22:47:01 +0100

    final

---
+++ server/app.js
@@ -0,0 +1,128 @@
+/**** External libraries ****/
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+/**** Configuration ****/
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static('../client01/build'));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+app.use(cors());
+
+const questionDB = require('./question_db')(mongoose);
+
+/**** Some test data ****/
+/*
+const data = [
+    {id:1,ques:"Is hentai safe to watch ?",answ: ["Absolutely","Believe it Fucking Believe it!"]},
+    {id:2,ques: "Do people die if they are killed?" , answ: ["Sometimes"  , "Its debatable"]}
+];
+data[0].answ[0].votes++; // question 0, answer 0
+data[0].answ[1].votes++; // question 0, answer 1
+data[1].answ[1].votes++; // question 1, answer 1
+console.log(data[0].answ[0]);
+const data = [
+    {id:1,ques:"Is hentai safe to watch ?", answ: [
+            {text: "Absolutely", votes: 0},
+            {text: "Believe it Fucking Believe it!", votes: 0}]
+    }
+];
+*/
+
+    /**** Routes ****/
+    app.get('/api/questions', async (req, res) =>
+    {
+        const questions = await questionDB.getQuestions();
+        res.json(questions);
+    });
+
+    app.get('/api/questions/:id', async (req, res) => {
+        let id = req.params.id;
+        const question = await questionDB.getQuestion(id);
+        res.json(question);
+    });
+
+    app.post('/api/questions', async (req, res) => {
+
+        let Question = {
+            ques: req.body.ques,
+            answ: []
+        };
+      const newQuestion = await questionDB.createQuestion(Question)
+        res.json(newQuestion);
+    });
+
+    app.post('/api/questions/:id', async (req, res) => {
+        const id = req.params.id;
+
+       // const answer = req.body.answ.text;
+      const answer =  {text: req.body.text , vote: req.body.vote};
+        const updateQuestion = await questionDB.addAnswer(id , answer);
+        res.json(updateQuestion);
+    });
+
+    app.put('api/question/:id' , async (req, res, next) => {
+        this.questionModel.findByIdAndUpdate(req.params.id, {
+            $set: req.body.vote
+        }, (error, data) => {
+            if (error) {
+                return next(error);
+                console.log(error);
+            } else {
+                res.json(data)
+                console.log('Answer voted for successfully !')
+            }
+        })
+    });
+/*
+app.post('/api/questions', (req, res) => {
+        const id = req.param.id;
+        const title = req.body.ques;
+        const desc = req.body.answ.text;
+        const nbm = Math.random()
+        const newQuestion = {
+            id: id,
+            ques: title,
+            answ: [{text: desc} , {vote:2}]
+        };
+        data.push(newQuestion);
+        res.json({msg: "Recipe added", newQuestion: newQuestion});
+    });
+
+    app.post('/api/recipes/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const text = req.body.text;
+        const ques = data.find(q => q.id === id);
+        ques.answ.push(text);
+        console.log(ques);
+        res.json({msg: "Answer added", ques: ques});
+    });
+*/
+    /* const id = req.params.id;
+    const recipe = data.find(e => e.id === parseInt(id));
+    recipe.answ = req.body.answ;
+    recipe.ques = req.body.ques;
+    res.json(recipe)*/
+
+app.get('*', (req, res) =>
+    res.sendFile(path.resolve('..', 'client', 'build', 'index.html'))
+);
+
+
+
+/**** Start! ****/
+//app.listen(port, () => console.log(`${appName} API running on port ${port}!`));
+const url = process.env.MONGO_URL || 'mongodb://localhost/question_db';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(async () => {
+      //  await questionDB.bootstrap(); // Fill in test data if needed.
+        await app.listen(port); // Start the API
+        console.log(`Question API running on port ${port}!`);
+    })
+    .catch(error => console.error(error));
---
+++ server/question_db.js
@@ -0,0 +1,76 @@
+class Db {
+    /**
+     * Constructors an object for accessing kittens in the database
+     * @param mongoose the mongoose object used to create schema objects for the database
+     */
+    constructor(mongoose) {
+
+        const questionSchema = new mongoose.Schema({
+            ques: String,
+            answ: [{text:String , vote:Number}]
+        });
+        this.questionModel = mongoose.model('question', questionSchema);
+
+    }
+
+    async getQuestions() {
+        try {
+            return await this.questionModel.find({});
+        } catch (error) {
+            console.error("getQuestions:", error.message);
+            return {};
+        }
+    }
+
+    async getQuestion(id) {
+        try {
+            return await this.questionModel.findById(id);
+        } catch (error) {
+            console.error("getQuestion:", error.message);
+            return {};
+        }
+    }
+
+    async createQuestion(newQuestion) {
+        // TODO: Error handling
+        let question = new this.questionModel(newQuestion);
+        return await question.save();
+    }
+
+    async addAnswer(questionId, answer ) {
+        // TODO: Error handling
+        const question = await this.getQuestion(questionId);
+
+
+        question.answ.push(answer);
+        return await question.save();
+    }
+
+    async editVote (questionId , vote) {
+        const question = await this.getQuestion(questionId);
+    }
+
+    /**
+     Function to create question if needed
+     */
+    /*
+    async bootstrap(count = 10) {
+        const q1 = new this.questionModel({
+            ques: "bam",
+            answ: [{text:"ddddddd" , vote:2}]
+        });
+
+        // Let's save it.
+        try {
+            let savedQ1 = await q1.save();
+
+            console.log("Questions saved.", savedQ1);
+        } catch(error) { // Error handling in case it doesn't save
+            console.error(error);
+        }
+    }
+    */
+}
+
+
+module.exports = mongoose => new Db(mongoose);
