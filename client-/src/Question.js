6d799420129ce1ddd9bed9466a1665cd2bc37e94
MichalMalina <38461440+MichalMalina@users.noreply.github.com>
2020-05-13 23:04:30 +0100

    c

---
+++ client-/src/Question.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Link, Router} from "@reach/router";
+import PostAnswer from "./PostAnswer";
+
+
+
+class Question extends Component {
+    render() {
+         const v = this.props.id;
+        //  const r = this.props.getQuestion(v);
+        //   const mapFunction = an =>
+        //        <li>{an}</li>;
+        //   let answers = r.answ.map(mapFunction);
+        const question = this.props.getQuestion(this.props.id);
+        let content = <p>Loading</p>;
+        if (question) {
+            content =
+                <>
+                    <div>
+                        <h1>{question.ques}</h1>
+                        <h3>Answers:</h3>
+                        <ul>
+                            {question.answ.map(a => <li key={a.text}>{a.text}</li>)}
+                        </ul>
+                        <h5><PostAnswer id={v} postAnswer={(id, text) => this.props.postAnswer(id, text)}/></h5>
+                        <Link to="/">Back</Link>
+                    </div>
+                </>
+
+        }
+            return content;
+        }
+}
+export default Question;
